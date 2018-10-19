70c2b44de3ecd1c6a6f3195a45b991e69f9b75d6
caravanaiesb <caravana.iesb@gmail.com>
2018-10-19 19:39:20 -0300

    Login Google OK

--- app/src/main/java/br/pi/iesb/LoginActivity.java
+++ app/src/main/java/br/pi/iesb/LoginActivity.java
@@ -2,7 +2,9 @@ package br.pi.iesb;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -10,23 +12,49 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
     private EditText edtEmail,edtSenha;
     private Button btnLogin,btnRegistrar;
     private TextView txtEsqueci;
     private FirebaseAuth auth;
+    private SignInButton btnGoogle;
+    private GoogleApiClient mGoogleApiClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
         inicializaComponentes();
         eventoClicks();
+        conectarGoogleApi();
     }
+
+    private void conectarGoogleApi() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+    }
+
     private void eventoClicks(){
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +82,47 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+        btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginGoogle();
+            }
+        });
+    }
+
+    private void LoginGoogle() {
+        Intent i = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+        startActivityForResult(i,1);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 1){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            if(result.isSuccess()){
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseLogin(account);
+            }
+        }
+    }
+
+    private void firebaseLogin(GoogleSignInAccount account) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                    if(task.isSuccessful()){
+                        Intent i = new Intent(LoginActivity.this,PrincipalActivity.class);
+                        startActivity(i);
+                    }
+                    else{
+                        alert("Email Ou Senha Invalidos");
+                    }
+                    }
+                });
     }
 
     private void login(String email, String senha) {
@@ -82,11 +151,18 @@ public class LoginActivity extends AppCompatActivity {
         btnLogin = (Button) findViewById(R.id.btnLogin);
         btnRegistrar= (Button) findViewById(R.id.btnRegistrar);
         txtEsqueci = (TextView) findViewById(R.id.txtEsqueci);
+        btnGoogle = (SignInButton) findViewById(R.id.btnGoogle);
+
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         auth = Dao.getFirebaseAuth();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        alert("Erro no Login");
     }
 }
--- app/src/main/java/br/pi/iesb/PrincipalActivity.java
+++ app/src/main/java/br/pi/iesb/PrincipalActivity.java
@@ -1,13 +1,125 @@
 package br.pi.iesb;
 
+import android.media.Image;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PrincipalActivity extends AppCompatActivity {
+import com.bumptech.glide.Glide;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class PrincipalActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+    private ImageView ivFoto;
+    private TextView ivEmail,ivID;
+    private Button btnSignOut;
+    private FirebaseAuth auth;
+    private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseUser firebaseUser;
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal);
+        inicializaCompontentes();
+        inicializarFireBase();
+        conectarGoogleApi();
+        eventoClicks();
+    }
+
+    private void eventoClicks() {
+        btnSignOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                signOut();
+            }
+        });
+    }
+
+    private void signOut() {
+        auth.signOut();
+
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                 alert("Conta Desconectada");
+                 finish();
+            }
+        });
+    }
+
+    private void conectarGoogleApi() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API,gso)
+                .build();
+    }
+
+    private void inicializarFireBase() {
+        auth = FirebaseAuth.getInstance();
+        authStateListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                firebaseUser = firebaseAuth.getCurrentUser();
+                if(firebaseUser!=null){
+                    exibirdados(firebaseUser);
+                }else{
+                    finish();
+                }
+            }
+        };
+    }
+
+    private void exibirdados(FirebaseUser firebaseUser) {
+        ivEmail.setText(firebaseUser.getEmail());
+        ivID.setText(firebaseUser.getUid());
+
+        Glide.with(PrincipalActivity.this).load(firebaseUser.getPhotoUrl()).into(ivFoto);
+    }
+
+    private void inicializaCompontentes() {
+        ivFoto = (ImageView) findViewById(R.id.ivFoto);
+        ivEmail = (TextView) findViewById(R.id.ivEmail);
+        ivID = (TextView) findViewById(R.id.ivId);
+        btnSignOut = (Button) findViewById(R.id.btnSignOut);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(authStateListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        auth.removeAuthStateListener(authStateListener);
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        alert("Falha na conexão");
+    }
+
+    private void alert(String s) {
+        Toast.makeText(PrincipalActivity.this,s,Toast.LENGTH_LONG).show();
     }
 }
