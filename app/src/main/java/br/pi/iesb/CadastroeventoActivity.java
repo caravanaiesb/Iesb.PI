80f2ab9cb8ac0cd718f32e8137cc4c3006bc2c0a
caravanaiesb <caravana.iesb@gmail.com>
2018-10-24 17:21:48 -0300

    Duvidoso

--- app/src/main/java/br/pi/iesb/CadastroeventoActivity.java
+++ app/src/main/java/br/pi/iesb/CadastroeventoActivity.java
@@ -1,15 +1,32 @@
 package br.pi.iesb;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ServerValue;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CadastroeventoActivity extends AppCompatActivity {
     private EditText txtDataEvento,txtNomeEvento,txtTipoEvento,txtAtracaoPrincipal;
+    private Button btnCancelarEvent,btnCadastrarEvent;
+    private FirebaseDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +35,57 @@ public class CadastroeventoActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         inicializaComponentes();
+        eventosClicks();
+        database=FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Eventos");
+        DatabaseReference refEventos = database.getReference("Eventos/Eventos");
+        refEventos.addValueEventListener(new ValueEventListener() {
 
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot snap : dataSnapshot.getChildren()) {
+                    Evento a = snap.getValue(Evento.class);
+                    Log.d("Evento", a.toString());
+
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void eventosClicks() {
+        btnCancelarEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnCadastrarEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Evento x = new Evento(txtNomeEvento.getText().toString(),txtTipoEvento.getText().toString(),txtDataEvento.getText().toString(),txtAtracaoPrincipal.getText().toString());
+                adicionarLista(x);
+
+            }
+        });
+    }
+
+
+
+    private void adicionarLista(Evento x) {
+        database=FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Eventos");
+        DatabaseReference newRef = myRef.child("Eventos").push();
+        newRef.setValue(x);
+
+    }
+
+    private void alert(String s) {
+        Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
     }
 
     private void inicializaComponentes() {
@@ -26,6 +93,8 @@ public class CadastroeventoActivity extends AppCompatActivity {
         txtDataEvento= (EditText) findViewById(R.id.txtDataEvento);
         txtNomeEvento= (EditText) findViewById(R.id.txtNomeEvento);
         txtTipoEvento= (EditText) findViewById(R.id.txtTipoEvento);
+        btnCadastrarEvent =(Button) findViewById(R.id.btnCadastrarEvent);
+        btnCancelarEvent = (Button) findViewById(R.id.btnCancelarEvent);
     }
 
 }
--- app/src/main/java/br/pi/iesb/FeedeventActivity.java
+++ app/src/main/java/br/pi/iesb/FeedeventActivity.java
@@ -3,31 +3,79 @@ package br.pi.iesb;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class FeedeventActivity extends AppCompatActivity {
 
     private RecyclerView listaDados;
+    private EventAdapter eventAdapter;
+    private FirebaseDatabase database;
+    private ArrayList<Evento> eventList;
+    private ArrayList<Evento> lista;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.feedevent);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        RecuperarLista();
+
+
+
+
+
+
         setSupportActionBar(toolbar);
         listaDados = (RecyclerView) findViewById(R.id.listaDados);
 
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        listaDados.setLayoutManager(linearLayoutManager);
+        List<Evento> dados = RecuperarLista();
+        eventAdapter = new EventAdapter(dados);
+        listaDados.setAdapter(eventAdapter);
         }
+
+    private ArrayList<Evento> RecuperarLista() {
+        database=FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Eventos");
+        DatabaseReference refEventos = database.getReference("Eventos/Eventos");
+        refEventos.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                eventList=dataSnapshot.getValue(Evento.class);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return eventList;
+    }
 
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -48,5 +96,6 @@ public class FeedeventActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 
 }
